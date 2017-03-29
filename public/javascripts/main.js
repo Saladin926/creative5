705451aebccf657659d97b642d4b12ce1acade5c
sethclaw10 <sethclaw10@gmail.com>
2017-03-29 23:13:07 +0000

    third commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,22 +1,86 @@
 var app = window.angular.module('app', ['chart.js']);
-app.controller('mainCtrl', mainCtrl);
+app.factory('heightInterfacer', heightInterfacer)
+app.controller('mainCtrl', mainCtrl)
+
+
+
+function heightInterfacer ($http) {
+
+  var API_ROOT = 'api/height'
+  return {
+    get: function () {
+      return $http
+        .get(API_ROOT)
+        .then(function (resp) {
+          return resp.data
+        })
+    },
+    post: function (formData) {
+      return $http
+         .post(API_ROOT,formData)
+         .then(function (resp) {
+           console.log("Post worked");
+         })
+    }
+  }
+}
+
+
+
 
 function mainCtrl ($scope) {
-
+  $scope.total = 10;
   $scope.height = {
-  	feet:6,
-  	inch:11,
+  	feet:5,
+  	inches:11,
+    total:0,
   	gender: "Male",
+  };
+  $scope.height.total = $scope.height.feet * 12 + $scope.height.inches;
+
+  $scope.submitHeight = function(){
+    console.log('submit');
+    console.log($scope.height);
   };
 
 
-  $scope.submitHeight = function() {/*
-    var formData = {name:$scope.Name,avatarUrl:$scope.Url};
-    console.log(formData);
-    pokemonFetcher.post(formData); // Send the data to the back end
-    $scope.pokemon.push(formData); // Update the model*/
-    console.log('submit');
-  }
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+
+  $scope.change = function(){
+    if($scope.height.inches >= 12){
+      var rem = $scope.height.inches % 12;
+      $scope.height.feet += ($scope.height.inches - rem)/12;
+      $scope.height.inches = rem;
+    }
+    $scope.height.total = $scope.height.feet * 12 + $scope.height.inches
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
 
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,53 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var mongodb = require('mongodb');
+var fs = require('fs');
+
+var MongoClient = mongodb.MongoClient;
+var dbUrl = 'mongodb://localhost:27017/heights';
+var collection;
+
+MongoClient.connect(dbUrl, function (err, db) {
+  
+  // we will use this variable later to insert and retrieve a "collection" of data
+  collection = db.collection('heights');
+  collection.remove(function(err){
+      if (err) return console.error(err);
+      console.log('collection cleared.');
+  });
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    // HURRAY!! We are connected. :)
+    console.log('Connection established to', dbUrl);
+    
+    collection.insert(heights, function (err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('Inserted documents into the "heights" collection. The documents inserted with "_id" are:', result.length, result);
+      }
+      
+      // Dont Close connection
+      // db.close()
+    })
+  }
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.get('/api/height', function(req, res) {
+	console.log('get');
+});
+
+router.post('/api/height', function(req, res) {
+	console.log('get');
 });
 
 module.exports = router;
