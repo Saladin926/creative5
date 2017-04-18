b4566e7c484c1775011135e4a1ee5fe0400f8850
David Ostler <davidtostler@gmail.com>
2017-04-17 23:35:50 -0600

    Added the user schema for mongoose and fixed the whitespace in the node JavaScript.

--- routes/index.js
+++ routes/index.js
@@ -14,28 +14,38 @@ var HeightSchema = new mongoose.Schema({
 });
 var Height = mongoose.model('Height',HeightSchema);
 
-//check if we already have the example data in the db
-Height.count({},function(err,count){ 
-        if (!count) {
-			fs.readFile('data/male.json', 'utf8', function (err, data) {
-				if (err) throw err;
-				console.log("Inserting male heights");
-				Height.collection.insert(JSON.parse(data),function(err,docs){
-					if (err) throw err;
-					console.log("Male heights successfully inserted")
-				});
-			});
+var UserSchema = new mongoose.Schema({
+  google           : {
+    id           : String,
+    token        : String,
+    email        : String,
+    name         : String
+  }
+});
+var User = mongoose.model('User',UserSchema);
 
-			fs.readFile('data/female.json', 'utf8', function (err, data) {
-				if (err) throw err;
-				console.log("Inserting female heights");
-				Height.collection.insert(JSON.parse(data),function(err,docs){
-					if (err) throw err;
-					console.log("Female heights successfully inserted")
-				});
-			});
-        }
+//check if we already have the example data in the db
+Height.count({},function(err,count){
+  if (!count) {
+    fs.readFile('data/male.json', 'utf8', function (err, data) {
+      if (err) throw err;
+      console.log("Inserting male heights");
+      Height.collection.insert(JSON.parse(data),function(err,docs) {
+        if (err) throw err;
+        console.log("Male heights successfully inserted")
+      });
     });
+
+    fs.readFile('data/female.json', 'utf8', function (err, data) {
+      if (err) throw err;
+      console.log("Inserting female heights");
+      Height.collection.insert(JSON.parse(data),function(err,docs) {
+        if (err) throw err;
+        console.log("Female heights successfully inserted");
+      });
+    });
+  }
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -43,18 +53,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/api/heights', function(req, res) {
-	Height.find({},function(err,heights){
-		if (err) throw err;
-		res.send(heights);
-	});
+  Height.find({},function(err,heights) {
+    if (err) throw err;
+    res.send(heights);
+  });
 });
 
 router.post('/api/height', function(req, res) {
-	var newHeight = new Height(req.body);
-	newHeight.save(function(err, post) {
-	    if (err) return console.error(err);
-	    res.sendStatus(200);
-	  });
+  var newHeight = new Height(req.body);
+  newHeight.save(function(err, post) {
+    if (err) return console.error(err);
+    res.sendStatus(200);
+  });
 });
 
 module.exports = router;
